6f6e4f41342e7438f814eb24afe4f3e44951a426
Gazmira <gazmira.brahushi1@gmail.com>
2022-05-23 11:01:31 -0400

    adding a a file

--- hello.py
+++ hello.py
@@ -1,2 +1,14 @@
 print("hello git")
 print("this is our first lab activity on AI in Enterprise")
+print("a basic program in python")
+  
+number1 = input("First number: ")
+number2 = input("\nSecond number: ")
+  
+# Adding two numbers
+# User might also enter float numbers
+sum = float(number1) + float(number2)
+  
+# Display the sum
+# will print value in float
+print("The sum of {0} and {1} is {2}" .format(number1, number2, sum))
