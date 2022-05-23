8029d88b4badb8d2bf20055306235ec8bb110a6d
Gazmira <gazmira.brahushi1@gmail.com>
2022-05-23 11:11:18 -0400

    adding a python file

--- hello.py
+++ hello.py
@@ -10,4 +10,4 @@ sum = float(number1) + float(number2)
   
 # Display the sum
 
-print("The sum of {0} and {1} is {2}" .format(number1, number2, sum))
+print("The sum of {0} and {1} is {2}" .format(x, y, sum))
